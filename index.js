8550a9f9865fa153c1694fc0552d8e3e62a41cc0
nhhasib <nafiul.hasan1997@gmail.com>
2023-06-21 23:12:11 +0600

    user role change api added

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
-const port = 5000
+var jwt = require('jsonwebtoken');
+require('dotenv').config()
+const port = process.env.PORT||5000
 
 app.use(cors())
-app.use(express())
+app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorization token' });
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unathorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 const uri = "mongodb+srv://dashboard:xggYwRwpsARJoviJ@cluster0.uld9vql.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,12 +41,92 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const usersCollection=client.db('Dashboard').collection('users')
+    const ittemsCollection=client.db('Dashboard').collection('items')
+
+    app.post('/jwt', (req,res) => {
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'});
+      res.send({token})
+    })
+    app.get("/allUsers", async (req, res) => {
+      const result =await usersCollection.find().toArray();
+    res.send(result)
+})
+
+    app.get('/user',verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([])
+      }
+          const decodedEmail = req.decoded.email;
+    if (email !== decodedEmail) {
+      return res.status(403).send({ error: true, message: 'forbidden access' })
+    }
+    const query = { email: email };
+    const result = await usersCollection.findOne(query);
+    res.send(result);
+    })
+
+    app.post("/users", async (req, res) => {
+      const data = req.body;
+      const query = { email: data.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(data);
+      res.send(result);
+    });
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const filter = { _id:new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+          $set: {
+              role:'admin'
+          },
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+  })
+  app.patch('/users/doctor/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const filter = { _id:new ObjectId(id) };
+    const options = { upsert: true };
+    const updateDoc = {
+        $set: {
+            role:'doctor'
+        },
+    }
+    const result = await usersCollection.updateOne(filter, updateDoc, options);
+    res.send(result)
+  })
+
+  app.patch('/users/customer/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const filter = { _id:new ObjectId(id) };
+    const options = { upsert: true };
+    const updateDoc = {
+        $set: {
+            role:'customer'
+        },
+    }
+    const result = await usersCollection.updateOne(filter, updateDoc, options);
+    res.send(result)
+})
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
